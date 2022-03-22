ef6543410697df874c9842efddf28745d58e16d9
Takahiro Ogawa <boroney1@gmail.com>
2022-03-22 13:07:26 +0900

    【4-1】【new】

--- 05_PHP_Basic/PHP_4-1.php
+++ 05_PHP_Basic/PHP_4-1.php
@@ -1,5 +1,13 @@
 <?php
 // 1~10 の数値をランダムで生成し、偶数か奇数かを判定して表示してください
 // 生成したランダム値も表示してください。※ランダム値生成：rand(1,10);
-
+function numCheck() {
+	$randomNum = rand(1,10);
+	if ($randomNum % 2 === 0) {
+		return "偶数: $randomNum";
+	} else {
+		return "奇数: $randomNum";
+	}
+}
+echo numCheck();
 ?>
