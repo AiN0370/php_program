6ade49bc17e4666697913eeb7150f88a8bd72d5e
小川貴大 <takahiro@MacBook.local>
2022-03-22 22:35:15 +0900

    【4-1】【retry】

--- 05_PHP_Basic/PHP_4-1.php
+++ 05_PHP_Basic/PHP_4-1.php
@@ -1,7 +1,7 @@
 <?php
 // 1~10 の数値をランダムで生成し、偶数か奇数かを判定して表示してください
 // 生成したランダム値も表示してください。※ランダム値生成：rand(1,10);
-$randomNum = rand(1,10);
+$randomNum = rand(1, 10);
 if ($randomNum % 2 === 0) {
   echo "偶数: $randomNum";
 } else {
