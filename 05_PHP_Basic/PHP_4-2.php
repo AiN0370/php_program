b3c23afd7fc9bf9c019257a1338cfb9167fa1dba
小川貴大 <takahiro@MacBook.local>
2022-03-24 12:50:13 +0900

    【4-1】【new】

--- 05_PHP_Basic/PHP_4-2.php
+++ 05_PHP_Basic/PHP_4-2.php
@@ -5,5 +5,22 @@
 // 70 点〜79 点　　良
 // 80 点〜99 点　　優
 // 100 点　　　　満点
-
+$score = rand(0, 100);
+switch ($score) {
+  case $score < 50:
+    echo "不可: $score <br>";
+    break;
+  case $score < 70:
+    echo "可: $score <br>";
+    break;
+  case $score < 80:
+    echo "良: $score <br>";
+    break;
+  case $score < 100:
+    echo "優: $score <br>";
+    break;
+  case ($score = 100):
+    echo "満点: $score <br>";
+    break;
+}
 ?>
