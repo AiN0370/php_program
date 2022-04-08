c0965bfff1363e0327012623d786f2293628191d
小川貴大 <takahiro@MacBook.local>
2022-04-09 08:30:33 +0900

    [PHP_A_1-2][new]初回提出

--- 06_PHP_Advanced/PHP_A_1-2.php
+++ 06_PHP_Advanced/PHP_A_1-2.php
@@ -1,5 +1,30 @@
 <?php
 // 【PHP_A_1-2】 users テーブルのデータを全件出力する
+$host = 'localhost';
+$user = 'autumn_tarou';
+$password = 'autumn';
+$dbname = 'mysql';
 
+// Set DSN
+$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
 
-?>
+// PDO Instance を作成
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    echo 'Connection successful';
+    // users table から情報を得て出力
+    $sql = 'SELECT * FROM users';
+    $users = $pdo->query($sql);
+    foreach ($users as $user) {
+        echo '<li>' .
+            $user['name'] .
+            ', ' .
+            $user['email'] .
+            ', ' .
+            $user['password'] .
+            '</li>';
+    }
+} catch (PDOException $error) {
+    // PDO connection エラーの場合メッセージ出力
+    echo $error->getMessage();
+}
