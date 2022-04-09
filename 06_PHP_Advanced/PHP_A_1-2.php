f41936f7fe69047e4347809b95e989f2fcb2f184
小川貴大 <takahiro@MacBook.local>
2022-04-09 22:14:00 +0900

    [PHP_A_1-2][fix]接続解除・フォーマティング

--- 06_PHP_Advanced/PHP_A_1-2.php
+++ 06_PHP_Advanced/PHP_A_1-2.php
@@ -12,19 +12,18 @@ $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
 try {
     $pdo = new PDO($dsn, $user, $password);
     echo 'Connection successful';
+    echo '<br>';
     // users table から情報を得て出力
     $sql = 'SELECT * FROM users';
     $users = $pdo->query($sql);
     foreach ($users as $user) {
-        echo '<li>' .
-            $user['name'] .
-            ', ' .
-            $user['email'] .
-            ', ' .
-            $user['password'] .
-            '</li>';
+        echo $user['name'] . ', ' . $user['email'] . ', ' . $user['password'];
+        echo '<br>';
     }
 } catch (PDOException $error) {
     // PDO connection エラーの場合メッセージ出力
     echo $error->getMessage();
+    die();
 }
+// 接続を閉じる
+$pdo = null;
