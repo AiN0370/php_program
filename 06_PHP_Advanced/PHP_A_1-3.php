87f2804e3d3e7d8696bc4c71612b6eecedf799f5
小川貴大 <takahiro@MacBook.local>
2022-04-12 21:54:47 +0900

    [PHP_A_1-3][new]初回提出

--- 06_PHP_Advanced/PHP_A_1-3.php
+++ 06_PHP_Advanced/PHP_A_1-3.php
@@ -1,5 +1,24 @@
 <?php
 // 【PHP_A_1-3】 orders テーブルから、ID が一番大きいものを削除する
+$host = 'localhost';
+$user = 'autumn_tarou';
+$password = 'autumn';
+$dbname = 'mysql';
 
+// PDO 接続
+$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+$pdo = new PDO($dsn, $user, $password);
+// SQL作成
+$stmt = $pdo->prepare('DELETE FROM orders WHERE id = :id');
+// 一番大きいID＝10のデータを削除
+echo 'Delete "id=10" <br>';
+$id = 10;
+$res = $stmt->execute(['id' => $id]);
+
+// 確認のためテーブルを出力
+$stmt = $pdo->query('SELECT * FROM orders');
+while ($order = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo $order['id'] . '<br>';
+}
 
 ?>
