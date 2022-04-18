dd256794113805beb45b121d317f2df69559e2c3
小川貴大 <takahiro@MacBook.local>
2022-04-18 21:33:31 +0900

    [PHP_A_2-2][fix]修正1.変数Name

--- 06_PHP_Advanced/PHP_A_2/PHP_A_2-2.php
+++ 06_PHP_Advanced/PHP_A_2/PHP_A_2-2.php
@@ -7,15 +7,15 @@
 // ]
 $json = file_get_contents('prefecture.json');
 $prefecture = json_decode($json, true);
-$prefecture2 = [];
+$newPrefecture = [];
 
 // 必要なデータを取得してprefecture2配列に格納
 foreach ($prefecture[0] as $key => $value) {
-    array_push($prefecture2, [$value['name'] => array_column($value['city'], 'city')]);
+    array_push($newPrefecture, [$value['name'] => array_column($value['city'], 'city')]);
 }
 // 課題の要件通り出力
 echo '<pre>';
-var_dump($prefecture2);
+var_dump($newPrefecture);
 echo '</pre>';
 
 ?>
