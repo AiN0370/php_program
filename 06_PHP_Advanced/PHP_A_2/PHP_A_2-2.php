aadce22d7c712c93b431bd05491fc57fcadb93fa
小川貴大 <takahiro@MacBook.local>
2022-04-18 15:52:09 +0900

    [PHP_A_2-2][new]初回提出

---
+++ 06_PHP_Advanced/PHP_A_2/PHP_A_2-2.php
@@ -0,0 +1,21 @@
+<?php
+// 【PHP_A_2-2】 読み込んだ json を下記の形式の配列にして表示させてください
+// [
+//     "都道府県名A" => [市町村名1, 市町村名2...],
+//     "都道府県名B" => [市町村名3, 市町村名4...],
+//     ...
+// ]
+$json = file_get_contents('prefecture.json');
+$prefecture = json_decode($json, true);
+$prefecture2 = [];
+
+// 必要なデータを取得してprefecture2配列に格納
+foreach ($prefecture[0] as $key => $value) {
+    array_push($prefecture2, [$value['name'] => array_column($value['city'], 'city')]);
+}
+// 課題の要件通り出力
+echo '<pre>';
+var_dump($prefecture2);
+echo '</pre>';
+
+?>
