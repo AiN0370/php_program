cd581af1ec237c4bf023192f4416667d275c8bd3
小川貴大 <takahiro@MacBook.local>
2022-04-18 12:16:52 +0900

    [PHP_A_2-1][retry]修正2

--- 06_PHP_Advanced/PHP_A_2/PHP_A_2-1.php
+++ 06_PHP_Advanced/PHP_A_2/PHP_A_2-1.php
@@ -1,8 +1,8 @@
 <?php
 // 【PHP_A_2-1】 json ファイルを読み込んで各都道府県名（キー名：name）を表示させてください
 $json = file_get_contents('prefecture.json');
-$arr = json_decode($json, JSON_OBJECT_AS_ARRAY);
-foreach ($arr[0] as $key => $value) {
+$prefecture = json_decode($json, true);
+foreach ($prefecture[0] as $key => $value) {
     echo '(' . $key . ': ' . $value['name'] . ')<br>';
 }
 ?>
