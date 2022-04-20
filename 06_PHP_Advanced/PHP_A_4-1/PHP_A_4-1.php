7a05d6106376083421c11ae18be76dd5bbe903b4
小川貴大 <takahiro@MacBook.local>
2022-04-20 22:28:30 +0900

    [PHP_A_4-1][new]初回提出

--- 06_PHP_Advanced/PHP_A_4-1/PHP_A_4-1.php
+++ 06_PHP_Advanced/PHP_A_4-1/PHP_A_4-1.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // 【PHP_A_4-1】 簡易ポーカーゲーム
 
 // 山札（データベース）から、自分と相手に5枚ずつカードを渡す。
@@ -10,8 +11,152 @@
 // 〜勝敗について〜
 // 役は、ワンペア、ツーペア、スリーカード、フォーカードで戦う。
 // ※絵柄については判定しなくて良いです。
+$host = 'localhost';
+$user = 'root';
+$password = 'root';
+$dbname = 'mysql';
 
+// PDO 接続
+$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+$pdo = new PDO($dsn, $user, $password);
 
+// セッションから手札を読み込み
+if (isset($_POST['shuffle_card'])) {
+    $player1 = [];
+    $player2 = [];
+} else {
+    $player1 = $_SESSION['player1'];
+    $player2 = $_SESSION['player2'];
+}
 
+// PDO Prepare
+$sql = 'SELECT * FROM trumps WHERE id = :id';
+$stmt = $pdo->prepare($sql);
+
+// 乱数用配列
+$rands = [];
+// カードを重複無しに二人のプレイヤーにランダムに配布
+if (isset($_POST['shuffle_card'])) {
+    for ($i = 0; $i < 5; $i++) {
+        while (true) {
+            // SQLのIDをランダムに選ぶ
+            $card = rand(1, 52);
+            $stmt->execute(['id' => $card]);
+            $post = $stmt->fetch();
+            // 重複がなかったらplayer1配列にIDのマークと番号を追加
+            if (!in_array($card, $rands)) {
+                array_push($player1, [
+                    'mark' => $post['mark'],
+                    'number' => intval($post['number']),
+                ]);
+                $rands[] = $card;
+                break;
+            }
+        }
+        while (true) {
+            $card = rand(1, 52);
+            $stmt->execute(['id' => $card]);
+            $post = $stmt->fetch();
+            // 重複がなかったらplayer2配列に追加
+            if (!in_array($card, $rands)) {
+                array_push($player2, [
+                    'mark' => $post['mark'],
+                    'number' => intval($post['number']),
+                ]);
+                $rands[] = $card;
+                break;
+            }
+        }
+    }
+}
+
+function markOutput($i)
+{
+    if ($i == 'spade') {
+        return '♠';
+    } elseif ($i == 'heart') {
+        return '♥';
+    } elseif ($i == 'diamond') {
+        return '♦';
+    } elseif ($i == 'club') {
+        return '♣';
+    }
+}
+
+// 勝負！ボタンを押したら強弱を判別する
+function setScore($player)
+{
+  // 数字とマークに分ける
+    $numbers = [];
+    $marks = [];
+    foreach ($player as $key => $value) {
+      $numbers[] = $value['number'];
+      $marks[] = $value['mark'];
+    }
+    // 同じ数字の数によってスコアのポイントを決める
+    $result = array_count_values($numbers);
+    $score = ['point' => 0, 'status' => []];
+
+    foreach ($result as $key => $value) {
+        if ($value == 5) {
+            $score['point'] += 5;
+            array_push($score['status'], 'ファイブカード');
+        } elseif ($value == 4) {
+            $score['point'] += 4;
+            array_push($score['status'], 'フォーカード');
+        } elseif ($value == 3) {
+            $score['point'] += 3;
+            array_push($score['status'], 'スリーカード');
+        } elseif ($value == 2 && array_count_values($marks) == 3) {
+            $score['point'] += 2;
+            array_push($score['status'], 'ツーペア');
+        } elseif ($value == 2) {
+            $score['point'] += 1;
+            array_push($score['status'], 'ワンペア');
+        }
+    }
+    if ($score['point'] == 0) {
+        array_push($score['status'], 'ノーペア');
+    }
+    return $score;
+}
+
+$score1 = setScore($player1);
+$score2 = setScore($player2);
+
+// 勝ったらYou Win、負けたらYou　Loesを表示する。
+function judgeScore($score1, $score2)
+{
+    if ($score1 > $score2) {
+        return 'You Win!!';
+    } elseif ($score1 < $score2) {
+        return 'You Lose!!';
+    } else {
+        return 'Draw';
+    }
+}
+
+// ワンペア、ツーペアなどを表示する
+function displayStatus($status)
+{
+    foreach ($status as $key => $value) {
+        echo $value . ' ';
+    }
+}
+// Win, Lose を記録する
+if (isset($_POST['judge_game'])) {
+    if ($score1 > $score2) {
+        $_SESSION['win'] += 1;
+    } elseif ($score1 < $score2) {
+        $_SESSION['lose'] += 1;
+    }
+}
+if (isset($_POST['reset'])) {
+    $_SESSION['win'] = 0;
+    $_SESSION['lose'] = 0;
+}
+// セッションに記録
+$_SESSION['player1'] = $player1;
+$_SESSION['player2'] = $player2;
 
 ?>
---
+++ 06_PHP_Advanced/PHP_A_4-1/index.php
@@ -0,0 +1,157 @@
+<?php include 'PHP_A_4-6.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>ポーカーゲームを実装！</title>
+    <!-- MDB -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css"
+      rel="stylesheet"
+    />
+    <!-- Bootstrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body >
+    <div class="shadow-lg card text-center w-75 d-flex m-auto mt-5">
+      <div class="card-header bg-danger fs-2 text-light font-weight-bold">ポーカーゲーム</div>
+      <div class="card-body">
+        <h5 class="card-title">あなたの手札</h5>
+        <!-- 手札 -->
+         <div class="px-5 py-4 d-flex justify-content-between">
+            <?php foreach ($_SESSION['player1'] as $key => $value) { ?>
+              <div class="card p-2 border-dark" style="width: 8rem; height: 12rem">
+              <h4 class="card-title fs-2"><?php echo markOutput(
+                  $value['mark']
+              ); ?></h4>
+              <div style="height: 2rem"></div>
+              <h3 class="card-text"><?php echo $value['number']; ?></h3>
+              </div>
+            <?php } ?>
+        </div> 
+        <!-- ここまで -->
+        <div
+          class="d-flex align-items-center justify-content-between m-auto"
+          style="width: 20%"
+        >
+          <form method="POST" action="">
+            <input
+            name="shuffle_card"
+            class="btn btn-secondary"
+            type="submit"
+            value="シャッフル" 
+            />
+          </form>
+          <form method="POST">
+            <input
+            name="judge_game"
+              class="btn btn-danger"
+              type="submit"
+              value="勝負！" 
+            />
+          </form>
+        </div>
+        <p class="fs-10 pt-3">勝敗結果</p>
+        <p class="fs-3 text-danger pb-3">
+          <?php if (isset($_POST['judge_game'])) {
+              echo judgeScore($score1['point'], $score2['point']);
+              echo ' ';
+              displayStatus($score1['status']);
+          } ?></p>
+        <p class="card-text">
+          <?php if (isset($_POST['judge_game'])) {
+              echo '【 相手の手札 】';
+              foreach ($_SESSION['player2'] as $key => $value) {
+                  echo $value['number'] . ', ';
+              }
+              displayStatus($score2['status']);
+          } ?>
+        </p>
+        
+        <p>
+          【　役による序列　】：ワンペア &lt; ツーペア &lt; スリーカード &lt;
+          フォーカード &lt; ファイブカード
+        </p>
+
+      </div>
+      <div
+        class="card-footer text-muted d-flex justify-content-between align-items-center bg-light"
+      >
+        <div class="d-flex">
+          <div class="mx-3">勝った回数：<?php echo $_SESSION['win']; ?>回</div>
+          <div class="mx-3">負けた回数：<?php echo $_SESSION['lose']; ?>回</div>
+        </div>
+        <div>
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-toggle="modal"
+            data-bs-target="#confirmModal"
+          >
+            リセット
+          </button>
+          <!-- Modal -->
+          <div
+            class="modal fade"
+            id="confirmModal"
+            tabindex="-1"
+            aria-labelledby="confirmModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="confirmModalLabel">
+                    勝敗履歴のリセット
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <pre style="white-space: pre-line; font-size: 16px">
+                                勝敗履歴をリセットします。
+                                よろしいですか？
+                  </pre>
+                  <div
+                    class="w-50 m-auto d-flex align-items-center justify-content-between"
+                  >
+                  <form action="" method="post" id="form1"> </form>
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      前に戻る
+                    </button>
+                    <button  class="btn btn-danger" name="reset" type="submit" value="reset" form="form1">
+                      リセット
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
